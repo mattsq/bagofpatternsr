912e5fa0f233090c28ea3e02efc1ca5b37abb7eb
Matthew Simmons <matthewsimmons@192-168-1-101.tpgi.com.au>
2020-10-24 16:06:18 +1100

    Add documentation for datasets.

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' FaceAll Data Set (Training)
+#'
+#' FaceAll was generated at UCR by Xiaopeng Xi. The faces belong to 14 grad students.
+#' Each facial outline is mapped onto a one dimensional series. Sourced from
+#' timeseriesclassification.com
+#'
+#' @format A data frame with 560 rows and 132 variables:
+#' \describe{
+#'   \item{attr%}{The position of the face at a point - 131 variables}
+#'   \item{target}{The student, as a class}
+#'   ...
+#' }
+#' @source \url{http://www.timeseriesclassification.com/description.php?Dataset=FaceAll}
+"FaceAll_TRAIN"
+
+#' FaceAll Data Set (Test)
+#'
+#' FaceAll was generated at UCR by Xiaopeng Xi. The faces belong to 14 grad students.
+#' Each facial outline is mapped onto a one dimensional series.
+#'
+#' @format A data frame with 1690 rows and 132 variables:
+#' \describe{
+#'   \item{attr%}{The position of the face at a point - 131 variables}
+#'   \item{target}{The student, as a class}
+#'   ...
+#' }
+#' @source \url{http://www.timeseriesclassification.com/description.php?Dataset=FaceAll}
+"FaceAll_TEST"
