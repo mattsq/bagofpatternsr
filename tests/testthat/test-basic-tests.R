cdff838454ba255040694cd67de6da79cdb53f85
Matthew Simmons <matthewsimmons@192-168-1-101.tpgi.com.au>
2020-10-24 15:59:37 +1100

    Fix the tests to be - for the package I'm actually using.

--- tests/testthat/test-basic-tests.R
+++ tests/testthat/test-basic-tests.R
@@ -1,12 +1,20 @@
 test_that("Basic model fit and predict works correctly", {
 
-    library(tsforest)
-    data("LargeKitchenAppliances_TRAIN")
-    data("LargeKitchenAppliances_TEST")
-    model <- tsforest(LargeKitchenAppliances_TRAIN)
-    preds <- predict(model, LargeKitchenAppliances_TEST)
-    preds <- preds$predictions
+    library(bagofpatternsr)
+    data("FaceAll_TRAIN")
+    data("FaceAll_TEST")
+    model <- bagofpatternsr::bagofpatterns_knn(FaceAll_TRAIN,
+                                               target = "target",
+                                               window_size = floor(sqrt(ncol(FaceAll_TRAIN))),
+                                               verbose = FALSE,
+                                               normalize = TRUE,
+                                               alphabet_size = 2,
+                                               PAA_number = 3)
 
-    expect_equal(length(preds), 375)
+    new_preds  <- predict(model,
+                          newdata = FaceAll_TEST,
+                          verbose = FALSE)
+
+    expect_equal(length(new_preds), 1690)
 
 })
